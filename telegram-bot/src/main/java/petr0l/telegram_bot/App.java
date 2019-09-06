5a3ea0ae22795479e48124761210ff9ebbd809b2
petr00l <54990231+petr00l@users.noreply.github.com>
2019-09-06 17:34:50 +0300

    telegram bot
    this is very good telebot

--- telegram-bot/src/main/java/petr0l/telegram_bot/App.java
+++ telegram-bot/src/main/java/petr0l/telegram_bot/App.java
@@ -1,13 +1,10 @@
 package petr0l.telegram_bot;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        //sdsd
     }
 }
